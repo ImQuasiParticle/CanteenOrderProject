530781370cc95063ec35bc63bf054b0e56253655
Prashastha01 <kovuriprashastha01@gmail.com>
2020-09-24 23:30:21 +0530

    remove functionality

--- src/components/customerComp/Book.js
+++ src/components/customerComp/Book.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { useState, useEffect, useCallback } from "react";
-import { db } from "../../firebase";
-import { Remove, Add } from "@material-ui/icons";
-import CustomerNav from "./CustomerNav";
-import firebase from "firebase";
-import { Typography } from "@material-ui/core";
-import Fastfood from "@material-ui/icons/Fastfood";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import React from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { db } from '../../firebase';
+import { Remove, Add } from '@material-ui/icons';
+import CustomerNav from './CustomerNav';
+import firebase from 'firebase';
+import { Typography } from '@material-ui/core';
+import Fastfood from '@material-ui/icons/Fastfood';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    justifyItems: "center",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+    justifyItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     height: 50,
     width: 150,
   },
   Bookpaper: {
-    justifyItems: "center",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+    justifyItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     width: 200,
   },
   add_remove: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     height: 30,
     width: 50,
   },
@@ -41,8 +41,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-//import Order from '../Order.js';
-const array = [];
 function Book({ user, setUser }) {
   const [cartItems, setCartItems] = useState([]);
   const [menu, setMenu] = useState([]);
@@ -51,7 +49,7 @@ function Book({ user, setUser }) {
   let [count, setCount] = useState(0);
 
   useEffect(() => {
-    db.collection("items").onSnapshot((snapshot) => {
+    db.collection('items').onSnapshot((snapshot) => {
       setMenu(
         snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -62,42 +60,39 @@ function Book({ user, setUser }) {
   }, []);
 
   const addItem = ({ id, item }) => {
-    if (cartItems.filter((item) => item.itemName == id).length == 0) {
+    if (cartItems.filter((item) => item.itemName === id).length === 0) {
       setCartItems((prevArray) =>
         prevArray.concat({ itemName: id, qty: 1, price: item.price })
       );
-      // array.push({ itemName: id, qty: 1, price: item.price });
     } else {
       let index = cartItems.findIndex((obj) => obj.itemName === id);
       console.log(index);
       cartItems[index].qty += 1;
       cartItems[index].price += item.price;
-      // cartItems.filter((add) => {
-      //   add.qty = add.qty + 1;
-      //   add.price += item.price;
-      //   // setCount(count + item.price);
-      // });
     }
     setCount(count + item.price);
-    // setCartItems(array);
   };
-  // console.log(cartItems);
-  const removeItem = (prop) => {
-    if (array.includes(prop)) {
-      var i = array.indexOf(prop);
-      array.splice(i, 1);
-      setCartItems(array);
-      console.log(array);
+  const removeItem = ({ id, item }) => {
+    let index = cartItems.findIndex((obj) => obj.itemName === id);
+    console.log(index);
+    if (index > 0 && cartItems[index].qty > 0) {
+      cartItems[index].qty -= 1;
+      cartItems[index].price -= item.price;
+      if (cartItems[index].qty === 0) {
+        cartItems.splice(index, 1);
+      }
     }
+
+    setCount(count - item.price);
   };
   function booking() {
-    db.collection("orders").add({
+    db.collection('orders').add({
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       name: user.displayName,
       email: user.email,
       orderItems: cartItems,
-      status: "Pending",
-      awaitStatus: "Receive",
+      status: 'Pending',
+      awaitStatus: 'Receive',
       totalAmount: count,
     });
     setCount(0);
@@ -111,17 +106,17 @@ function Book({ user, setUser }) {
           <div>
             <Grid container className={classes.root} spacing={2}>
               <Grid item xs={12}>
-                <Grid container justify="center" spacing={1}>
+                <Grid container justify='center' spacing={1}>
                   <Grid item>
-                    <Paper alignItems="center" className={classes.add_remove}>
+                    <Paper alignItems='center' className={classes.add_remove}>
                       <center>
-                        <Remove onClick={() => removeItem(id, item)} />
+                        <Remove onClick={() => removeItem({ id, item })} />
                       </center>
                     </Paper>
                   </Grid>
                   <Grid item>
                     <Paper className={classes.paper}>
-                      <Grid item alignItems="center">
+                      <Grid item alignItems='center'>
                         {id}
                         <br />
                         {item.price}/-
@@ -129,7 +124,7 @@ function Book({ user, setUser }) {
                     </Paper>
                   </Grid>
                   <Grid item>
-                    <Paper alignItems="center" className={classes.add_remove}>
+                    <Paper alignItems='center' className={classes.add_remove}>
                       <center>
                         <Add
                           onClick={() => {
@@ -150,7 +145,7 @@ function Book({ user, setUser }) {
           <h6>Your Meal</h6>
           <Paper className={classes.Bookpaper}>
             {cartItems.map((item) => (
-              <Typography variant="body1">
+              <Typography variant='body1'>
                 {item.itemName} Qty - {item.qty}
               </Typography>
             ))}
